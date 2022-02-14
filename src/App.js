a242b07a9fe79be226147d12fcd4f5218d643dda
Chase <>
2022-02-13 22:12:49 -0600

    A: Comments

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 // import { AiOutlineEdit } from 'react-icons/ai';
 
 function App() {
+
+    // Using a class for todo items gives us an API to follow for updating state
     class TodoItem {
         constructor(key, text) {
             this.key = key;
@@ -11,16 +13,21 @@ function App() {
         }
     }
 
+    // Using an object to hold the todo items and the number of created items
+    // allows us to use the keys as the ids for the todo items
     let [state, setState] = useState({ items: [], creationCount: 0 });
     window.state = state;
 
+    // Explicit add and remove functions makes the code easier to read, follow,
+    // and maintain in the future
     function addTodoItem(string) {
+        // This is a cleaner way to update the state than what you had before
         state.items.push(new TodoItem(state.creationCount, string));
         state.creationCount++;
         setState({ ...state });
     }
-
     function removeTodoItem(key) {
+        // Let me know if you have any questions about these functions
         state.items = state.items.filter((item) => item.key !== key);
         setState({ ...state });
     }
@@ -33,6 +40,7 @@ function App() {
 
             <div id="list-items-container">
                 {state.items.map(todoItem =>
+                    // We can now use the key to identify specific todo items
                     <div className="list-item" key={todoItem.key}>
                         <a onClick={() => removeTodoItem(todoItem.key)}>{todoItem.text}</a>
                     </div>
