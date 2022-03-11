6a798287ef611e1b9eb9102d74062fcc69b25276
chromey85 <yayyub85@gmail.com>
2022-03-11 09:53:14 +0000

    edits

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,44 +1,50 @@
 import react, { useState, useEffect, useRef } from "react";
-import './App.css';
-import { MdDeleteOutline } from 'react-icons/md';
-import { AiOutlineEdit } from 'react-icons/ai';
+import "./App.css";
+import { MdDeleteOutline } from "react-icons/md";
+import { AiOutlineEdit } from "react-icons/ai";
 
 function App() {
-    const [items, setItems] = useState([]);
-    // const addList = list => {
-    //     if (!list.text || /^\s*$/.test(list.text)) {
-    //         return;
-    //     }
+  const [items, setItems] = useState([]);
+  // const addList = list => {
+  //     if (!list.text || /^\s*$/.test(list.text)) {
+  //         return;
+  //     }
 
-    //     const newList = [list, ...items];
+  //     const newList = [list, ...items];
 
-    //     setItems(newList);
-    //     console.log(...items);
-    // }
+  //     setItems(newList);
+  //     console.log(...items);
+  // }
 
-    return (
-        <div className="all">
-            <h1>Shopping List</h1>
-            <h2>(To Delete, Click on the Item)</h2>
-            <div id="list_items_container">
-                {items.map((item, index) => (
-                    <div id="list_items">
-                        <a onClick={() => setItems(items.filter((_, ind) => ind !== index))}>
-                            {item}
-                        </a>
-                    </div>
-                ))}
-            </div>
-            <form onSubmit={(e) => {
-                e.preventDefault();
-                const formData = new FormData(e.currentTarget);
-                setItems((items) => [...items, formData.get("todo")])
-                e.currentTarget.reset();
-            }}>
-                <input name="todo" required />
-                <button className="submitButton" type="submit">Add to List</button>
-            </form>
-            {/* <form className="list_form" onSubmit={addItem}>
+  return (
+    <div className="all">
+      <h1>Shopping List</h1>
+      <h2>(To Delete, Click on the Item)</h2>
+      <div id="list_items_container">
+        {items.map((item, index) => (
+          <div id="list_items">
+            <a
+              onClick={() => setItems(items.filter((_, ind) => ind !== index))}
+            >
+              {item}
+            </a>
+          </div>
+        ))}
+      </div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const formData = new FormData(e.currentTarget);
+          setItems((items) => [...items, formData.get("todo")]);
+          e.currentTarget.reset();
+        }}
+      >
+        <input name="todo" required />
+        <button className="submitButton" type="submit">
+          Add to List
+        </button>
+      </form>
+      {/* <form className="list_form" onSubmit={addItem}>
                 <input className="list_input" type="text" placeholder="Enter Item" value={input} name={text} />
                 <div className="icons">
                     <MdDeleteOutline onClick={removeListItem} className="delete-icon" />
@@ -46,8 +52,8 @@ function App() {
                 </div>
                 <button className="add_button">Add to List</button>
             </form> */}
-        </div>
-    )
+    </div>
+  );
 }
 
-export default App
+export default App;
